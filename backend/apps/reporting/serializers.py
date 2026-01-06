10c17a5e158cf477d8c41db77507a225daa87309
munaim <muanIM@pmc.edu.pk>
2026-01-06 19:00:38 +0500

    kk

--- backend/apps/reporting/serializers.py
+++ backend/apps/reporting/serializers.py
@@ -5,6 +5,7 @@ class ReportSerializer(serializers.ModelSerializer):
     accession = serializers.CharField(source="study.accession", read_only=True)
     patient_name = serializers.CharField(source="study.patient.name", read_only=True)
     service_name = serializers.CharField(source="study.service.name", read_only=True)
+    study_id = serializers.UUIDField(source="study.id", read_only=True)
 
     class Meta:
         model = Report
